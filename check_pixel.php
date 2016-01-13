502fef55faa6d72a5731ef76dac810ce41214d69
Martono Wibowo <martonowibowo@gmail.com>
2016-01-13 17:55:14 +0700

    fix parse xml php

--- check_pixel.php
+++ check_pixel.php
@@ -7,15 +7,11 @@ foreach ($xmlimages as $xmlimage) {
 	// print_r($xmlimage);
 	foreach ( $xmlimage as $image ) {
 		// print_r($image);
-		if($image->pixel_height > 600) { 
-			// echo $image->image_url_https . ' => ' . $image->pixel_height . '<br/>';
-			echo '<img src="' . $image->image_url_https . '" width="600" /> <br/>';
+		if($image->pixel_height > 600 && $image->pixel_height < 2000 ) { 
+			echo $image->image_url_https . ' => ' . $image->pixel_height . '<br/>';
 		}
 	}
 }
-//$xml = simplexml_load_string('$xmlfile');
-//$json = json_encode($xml);
-//$array = json_decode($json,TRUE);
-//print_r($array);
+
 ?>
-</pre>
+<pre>
