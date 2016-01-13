b97c451ddb588deb39d5d1eaaea68fdd99ff4f87
Martono Wibowo <martonowibowo@gmail.com>
2016-01-13 13:56:49 +0700

    for each array

--- check_pixel.php
+++ check_pixel.php
@@ -19,33 +19,30 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
       list($width, $height, $type, $attr) = getimagesize($row["url"]);
-	  echo "Height: " .$height. "url: " . $row["url"]. "<br />";
+	  //echo "Height: " .$height. "url: " . $row["url"]. "<br />";
 	 
 	 if ($height > 100 && $height < 400)
 	 {
 		 $data[] = $row["url"];
 	 }
 	 
-	 
 	  
-	  //$sql_insert = 'INSERT INTO url_fix (url) VALUES ("'. $row[url] .'")';
-		//mysqli_query($conn, $sql_insert);
-	  //echo "url: " . $row["url"]. "Width: " .$width."<br>" ;
     }
 } else {
     echo "0 results";
 }
-
 mysqli_close($conn);
-print_r($data);
+//do wget in server	
+foreach($data as $value){ 
+//$output = shell_exec('wget '$value'');
+    echo $value; 
+?>
+</br>
+<?php
+}
+//$output = shell_exec('ls -lart');
+//echo "<pre>$output</pre>";
 
-
-//list($width, $height, $type, $attr) = getimagesize("http://h10003.www1.hp.com/digmedialib/prodimg/lowres/c04898865.png");
-
-//echo "Width: " .$width. "<br />";
-//echo "Height: " .$height. "<br />";
-//echo "Type: " .$type. "<br />";
-//echo "Attribute: " .$attr. "<br />";
 //$img = get_headers("http://h10003.www1.hp.com/digmedialib/prodimg/lowres/c04898865.png", 1);
 //print $img["Content-Length"];
 
