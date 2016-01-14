3db4e34be83b8563a0b2e4aabcc32c2a44f9a40c
Martono Wibowo <martonowibowo@gmail.com>
2016-01-14 14:01:16 +0700

    change parse_xml.sh to parse.sh and fix multiplexml

--- check_pixel.php
+++ check_pixel.php
@@ -2,52 +2,34 @@
 <body>
 <pre>
 <?php
-
-$xmlfile = simplexml_load_file('T8V72PA.xml');
+exec("ls -a | grep .xml",$output);
+$fp = fopen('url.file', 'w');
+foreach($output as $xmlfle)
+{
+//Parse XML File-----------------------------------//
+$xmlfile = simplexml_load_file($xmlfle);
 $xmlsku=$xmlfile->content->system->product_numbers->prodnum;
 echo $xmlsku;
 $xmlimages = $xmlfile->images;
 // print_r($xmlfile->images->image);
-$fp = fopen('url.file', 'w');
+
 foreach ($xmlimages as $xmlimage) {
 	// print_r($xmlimage);
 	foreach ( $xmlimage as $image ) {
 		// print_r($image);
 		if($image->pixel_height > 600 && $image->pixel_height < 2000 ) { 
 			//echo $image->image_url_http . ' => ' . $image->pixel_height . '<br/>';
-			$url = "wget $image->image_url_http\n";
+			$url = "wget -P $xmlsku $image->image_url_http\n";
 			//echo $url;
-		  
 		  fwrite($fp, $url);
-          
-			//$cmd = "wget ".$url."";
-			
-			//file_put_contents($img, file_get_contents($url));
 		}
 	}
 }
-
-
-fclose($fp);
-
-/*$command="chmod +x url.file";
-exec($command, $output);
-exec("./url.file");
-
-/*$cmd = "./url.file";
-
-while (@ ob_end_flush()); // end all output buffers if any
-
-$proc = popen($cmd, 'r');
-echo '<pre>';
-while (!feof($proc))
-{
-    echo fread($proc, 4096);
-    @ flush();
 }
-echo '</pre>';
-*/
+fclose($fp);
 echo "<pre>download complete</pre>";
+//---------------------------------------------------------------------------------------//
+
 ?>
 </pre>
 </body>
--- parse_xml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-file="$1"
-xgrep -x "//pixel_height" $file | awk -vRS="</pixel_height>" '{gsub(/.*<pixel_height>/,"");print}' > pixel.file 
-xgrep -x "//image_url_http" $file | awk -vRS="</image_url_http>" '{gsub(/.*<image_url_http>/,"");print}' >> url.file
-head -n -4 pixel.file > temp.file ; mv temp.file pixel.file
-sleep 2
-head -n -4 url.file > temp.file ; mv temp.file url.file
-
